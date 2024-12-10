b608804cb5d00f9113aa5a1472e9bccdfed2246c
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-12-10 10:43:11 +0300

    Generate colormap buffer and texture only if the colormap is not empty

--- src/ColorMap.cpp
+++ src/ColorMap.cpp
@@ -3,12 +3,14 @@
 ColorMap::ColorMap (std::vector<glm::vec4> colormap) {
     this->count = colormap.size();
 
-    glGenBuffers (1, &this->bufferID);
-    glBindBuffer (GL_TEXTURE_BUFFER, this->bufferID);
-    glBufferData (GL_TEXTURE_BUFFER, sizeof (glm::vec4) * this->count,
-                  &colormap[0][0], GL_STATIC_DRAW);
+    if (this->count > 0) {
+        glGenBuffers (1, &this->bufferID);
+        glBindBuffer (GL_TEXTURE_BUFFER, this->bufferID);
+        glBufferData (GL_TEXTURE_BUFFER, sizeof (glm::vec4) * this->count,
+                      &colormap[0][0], GL_STATIC_DRAW);
 
-    glGenTextures (1, &this->textureID);
-    glBindTexture (GL_TEXTURE_BUFFER, this->textureID);
-    glTexBuffer (GL_TEXTURE_BUFFER, GL_RGBA32F, this->bufferID);
+        glGenTextures (1, &this->textureID);
+        glBindTexture (GL_TEXTURE_BUFFER, this->textureID);
+        glTexBuffer (GL_TEXTURE_BUFFER, GL_RGBA32F, this->bufferID);
+    }
 }
